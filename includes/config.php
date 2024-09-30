7858f6d7830ed5182ace9d22b148e158a3e9ed52
duduBmoon21 <eleanortefera21@gmail.com>
2024-09-30 00:20:23 -0700

    Update

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','usermaanegemt');
+define('DB_NAME','ducodeep_usermaanegemt');
 // Establish database connection.
 try
 {
